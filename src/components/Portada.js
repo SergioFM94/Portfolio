5c9de5f08421b93dcb9fa11f3651234b5445651c
Sergio Fernandez <75262614+SergioFM94@users.noreply.github.com>
2023-04-26 00:41:40 +0200

    Añadir imagenes finales y favicon

--- src/components/Portada.js
+++ src/components/Portada.js
@@ -4,22 +4,22 @@ import "./Portada.css"
 export const Portada = () => {
   return (
     <section className="portada">
-        <div className="portada-contenido">
-            <h1 className="portada-titulo">Sergio Fernández Muyor</h1>
-            <span className="portada-slogan">Disciplina, Trabajo y crecimiento constante.</span>
-            <a href="#" className="portada-contacto boton-hover">Contacto</a>
-            <div className="portada-social-media">
-                <a href="https://www.linkedin.com/in/sergio-fern%C3%A1ndez-muyor-front-end/" className="link-hover">Linkedin</a>
-                <a href="https://github.com/SergioFM94/" className="link-hover">Github</a>
-                <a className="portada-boton-cv boton-hover" 
-                   download 
-                   href="Resume-Sergio-Fernández-Muyor.pdf"
-                > 
-                  Mi CV
-                </a>
-            </div>
+      <div className="portada-contenido">
+        <h1 className="portada-titulo">Sergio Fernández Muyor</h1>
+        <span className="portada-slogan">Disciplina,trabajo y crecimiento constante.</span>
+        <a href="#" className="portada-contacto boton-hover">Contacto</a>
+        <div className="portada-social-media">
+          <a href="https://www.linkedin.com/in/sergio-fern%C3%A1ndez-muyor-front-end/" className="link-hover">Linkedin</a>
+          <a href="https://github.com/SergioFM94/" className="link-hover">Github</a>
+          <a className="portada-boton-cv boton-hover"
+            download
+            href="Resume-Sergio-Fernández-Muyor.pdf"
+          >
+            Mi CV
+          </a>
         </div>
-        <img src="principal.png" alt="imagen portada" className="portada-imagen"/>
+      </div>
+      <img src="principal.png" alt="imagen portada" className="portada-imagen" />
     </section>
   )
 }
--- src/components/SobreMi.js
+++ src/components/SobreMi.js
@@ -4,31 +4,31 @@ import LineaColor from "./LineaColor"
 export const SobreMi = () => {
   return (
     <section className="sobre-mi" id="sobre-mi">
-        <div className="sobre-mi-contenido-titulo">
-          <h2 className="sobre-mi-titulo">Sobre mí</h2>
-          <LineaColor color='#33FFA9'/>
+      <div className="sobre-mi-contenido-titulo">
+        <h2 className="sobre-mi-titulo">Sobre mí</h2>
+        <LineaColor color='#33FFA9' />
+      </div>
+      <div className="sobre-mi-contenido">
+        <div >
+          <p className="sobre-mi-descripcion">
+            Hola, vivo en Almería, tengo más de 7 años de experiencia trabajando
+            en una empresa de Telecomunicaciones desempeñando el rol de soporte TI,
+            eventualmente gestionaba bases de datos. De forma paralela, he continuado estudioando
+            desarrollo de aplicaciones web.
+            <br /> <br />
+            Me encanta trastear hardware y software retro y ver
+            como se apañaban los desarrolladores en la época para superar las limitaciones de
+            hardware.
+          </p>
+          <a
+            href="https://www.linkedin.com/in/sergio-fern%C3%A1ndez-muyor-front-end/"
+            className="sobre-mi-boton boton-hover"
+          >
+            Perfíl de Linkedin {["/>"]}
+          </a>
         </div>
-        <div className="sobre-mi-contenido">
-          <div >
-            <p className="sobre-mi-descripcion">
-              Hola, vivo en Almería, tengo más de 7 años de experiencia trabajando
-              en una empresa de Telecomunicaciones desempeñando el rol de soporte TI,
-              eventualmente gestionaba bases de datos. De forma paralela, he continuado estudioando
-              desarrollo de aplicaciones web. 
-              <br/> <br/>
-              Me encanta trastear hardware y software retro y ver
-              como se apañaban los desarrolladores en la época para superar las limitaciones de
-              hardware.
-            </p>
-            <a 
-              href="https://www.linkedin.com/in/sergio-fern%C3%A1ndez-muyor-front-end/"
-              className="sobre-mi-boton boton-hover"
-            >
-              Perfíl de Linkedin {["/>"]}
-            </a>
-          </div>
-          <img src="principal.png" alt="imagen portada" className="sobre-mi-imagen"/>
-        </div>
+        <img src="principal2.png" alt="imagen personal" className="sobre-mi-imagen" />
+      </div>
 
     </section>
   )
